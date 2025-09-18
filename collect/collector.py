d66aaf428724fb35c5807d3a8b3c2a851acce9f8
aymen <aymenfkir23@gmail.com>
2025-09-18 15:04:48 +0100

    modifed the collector added user id and shop id

--- collect/collector.py
+++ collect/collector.py
@@ -14,27 +14,30 @@ logging.basicConfig(level=logging.INFO)
 ##test this file
 
 class Collector:
-    def __init__(self,url:str,apiKey,client:Client,bucket_name:str,path:str):
+    def __init__(self,url:str,apiKey,sburl:str,sbkey:str,bucket_name:str,path:str):
         self.url = url
         self.apiKey = apiKey
-        self.client = client
+        self.client = create_client(sburl,sbkey)
         self.bucket_name = bucket_name
         self.path = path
-        self.ids = [str(uuid.uuid4()) for _ in range(10_000)]
+        self.ids = [str(uuid.uuid4()) for _ in range(5_000)]
         self.shop_ids = [f"shop_{i}" for i in range(10_000)]
 
-        
-    def getData(self):
+
+    def getData(self) -> list[dict]|None:
+        result = None
         try:
             response = requests.get(self.url, headers={"X-API-Key": self.apiKey})
             response.raise_for_status()
-            return response.json()
+            result = response.json()
         except requests.exceptions.HTTPError as http_err:
             raise Exception(f"HTTP error occurred: {http_err}")
         except requests.exceptions.RequestException as req_err:
             raise Exception(f"Request failed: {req_err}")
         except Exception as e:
             raise Exception(f"Unexpected error: {e}")
+        finally :
+            return result
 
     def addUsers(self,data:pl.DataFrame)->pl.DataFrame:
 
@@ -44,15 +47,16 @@ class Collector:
             self.ids,dtype=pl.String
         ).alias("id")
 
-        num_unique_dates_in_range = base_ids.len()
+        num_unique_in_range = base_ids.len()
 
         row_indices = pl.Series("row_index", range(num_rows_df))
-        repeated_date_indices = row_indices % num_unique_dates_in_range
+        repeated_indices = row_indices % num_unique_in_range
 
         generated_ids_column = pl.Series(
             "id",
-            [base_ids[i] for i in repeated_date_indices]
+            [base_ids[i] for i in repeated_indices]
         )
+        generated_ids_column = generated_ids_column.shuffle(seed=42)
 
         # Add this new series as a new column to the DataFrame
         df_with_dates = data.with_columns(generated_ids_column)
@@ -66,15 +70,16 @@ class Collector:
             self.shop_ids,dtype=pl.String
         ).alias("shop_id")
 
-        num_unique_dates_in_range = base_ids.len()
+        num_unique_in_range = base_ids.len()
 
         row_indices = pl.Series("row_index", range(num_rows_df))
-        repeated_shops_indices = row_indices % num_unique_dates_in_range
+        repeated_shops_indices = row_indices % num_unique_in_range
 
         generated_shop_ids_column = pl.Series(
             "shop_id",
             [base_ids[i] for i in repeated_shops_indices]
         )
+        generated_shop_ids_column = generated_shop_ids_column.shuffle(seed=24)
 
         # Add this new series as a new column to the DataFrame
         df_with_dates = data.with_columns(generated_shop_ids_column)
@@ -94,31 +99,39 @@ class Collector:
         except Exception as e:
             raise e
          
-    def run_once(self):
+    def run_once(self)->None:
         data = self.getData()
-        self.upload(data)
-        logging.info("Upload successful")
+        if data:
+            self.upload(data)
+            logging.info("Upload successful")
+        else:
+            logging.warning("No data retrieved from the API.")
 
 
-    def run_loop(self, max_size=10):
+    def run_loop(self, max_size=10)->None:
         data = []
         counter = 0
         while True:
-            time.sleep(0.3)
-            data.extend(self.getData())
-            counter += 1
-            if counter >= max_size:
-                data = self.addUsers(pl.DataFrame(data))
-                data = self.addShops(data)
-                data = data.to_dicts()
-                self.upload(data)
-                logging.info("Upload successful")
-                data.clear()
-                counter = 0 
+            result = self.getData()
+            if result:
+                data.extend(result)
+                counter += 1
+                if counter >= max_size:
+                    data = self.addUsers(pl.DataFrame(data))
+                    data = self.addShops(data)
+                    data = data.to_dicts()
+                    self.upload(data)
+                    logging.info("Upload successful")
+                    data.clear()
+                    counter = 0 
+            else:
+                logging.warning("No data retrieved from the API.")
+                break
 
-    def main(self,loop=True,max_size=10):
+
+    def main(self,loop=True,max_size=10)->None:
         if loop:
-            self.run_loop()
+            self.run_loop(max_size=max_size)
         else:
             self.run_once()
 
@@ -131,19 +144,17 @@ if __name__ == "__main__":
         apiKey: str|None = os.getenv("api_key")
         if not url or not key:
             raise ValueError("Missing environment variable: project_url or project key")
-        client = create_client(url,key)
 
-        script_current_dir = os.path.dirname(os.path.abspath(__file__))
-        main_dir = os.path.dirname(script_current_dir)
-
-        with open(os.path.join(main_dir,"config.yaml")) as file:
+        with open("../config.yaml") as file:
             config = yaml.safe_load(file)
+        
         extract = Collector(
             url=config["mockaroo"]["url"],
             apiKey=apiKey,
-            path=config["supabase"]["path_raw_data"],
+            sburl=url,
+            sbkey=key,
+            path=config["supabase"]["path_raw_data"]["new"],
             bucket_name=config["supabase"]["bucketName"],
-            client=client
             )
         
         extract.main()
